1eb8ec3a29caa03087c96ee8b86fbfcc2b5e0d49
Shaleem Malik <shaleemmalik6@gmail.com>
2020-07-19 20:32:19 -0400

    final commit

---
+++ public/assets/js/updateInfo 2.js
@@ -0,0 +1,383 @@
+//Box1 hospital addres
+
+const FL1 = $("#box1_hosp_address").val();
+const FL1_a = $("box1_hosp").val();
+const FL1_b = $("#box1_city").val();
+const FL1_c = $("#box1_state").val();
+const FL1_d = $("#box1_zipcode").val();
+
+//Box1 hospital addres
+
+const FL2 = $("#box2_hosp_address").val();
+const FL2_a = $("#box2_street").val();
+const FL2_b = $("#box2_city").val();
+const FL2_c = $("#box2_state").val();
+const FL2_d = $("#box2_zipcode").val();
+
+//box 3a data
+
+const FL3_a = $("#box_3a_data").val();
+
+//box 3a data
+const FL3_b = $("#box_3b_data").val();
+
+//box 4 data
+const FL4 = $("#box_4_data").val();
+
+//box 5 data
+const FL5 = $("#fed_tax").val();
+
+//box 6 data
+
+const FL6_a = $("#box6_from").val();
+const FL6_b = $("#box6_through").val();
+
+//box 7 data
+
+const FL7 = $("#box_7").val();
+
+//box 8 data
+
+const FL8_a = $("#patient_id").val();
+const FL8_b = $("#box_8b").val();
+
+//box 9 data
+
+const FL9_a = $("#patient_address").val();
+const FL9_b = $("#box_9b").val();
+const FL9_c = $("#box_9c").val();
+const FL9_d = $("#box_9d").val();
+const FL9_e = $("#box_9e").val();
+
+//box 10 data
+const FL10 = $("#box_10_data").val();
+//box 11 data
+const FL11 = $("#box_11_data").val();
+//box 12 data
+const FL12 = $("#box_12").val();
+//box 13 data
+const FL13 = $("#box_13_data").val();
+//box 14 data
+const FL14 = $("#box_14_data").val();
+//box 15 data
+const FL15 = $("#box_15_data").val();
+//box 16 data
+const FL16 = $("#box_16_data").val();
+//box 17 data
+const FL17 = $("#box_17_data").val();
+//box 18 data
+const FL18 = $("#box_18_data").val();
+//box 19 data
+const FL19 = $("#box_19_data").val();
+//box 20 data
+const FL20 = $("#box_20_data").val();
+//box 21 data
+const FL21 = $("#box_21_data").val();
+//box 22 data
+const FL22 = $("#box_22_data").val();
+//box 23 data
+const FL23 = $("#box_23_data").val();
+//box 24 data
+const FL24 = $("#box_24_data").val();
+//box 25 data
+const FL25 = $("#box_25_data").val();
+//box 26 data
+const FL26 = $("#box_26_data").val();
+
+//box 27 data
+const FL27 = $("#box_27_data").val();
+
+//box 28 data
+
+const FL28 = $("#box_28_data").val();
+
+//box 29 data
+
+const FL29 = $("#box_29_data").val();
+
+//box 30 data
+
+const FL30 = $("#box_30_data").val();
+
+//box 31 data
+const FL31_a = $("#box_31_code").val();
+const FL31_b = $("#box_31b_code").val();
+const FL31_a1 = $("#box_31_date").val();
+const FL31_b1 = $("#box_31b_date").val();
+
+//box 32 data
+const FL32_a = $("#box_32_code").val();
+const FL32_b = $("#box_32b_code").val();
+const FL32_a1 = $("#box_32_date").val();
+const FL32_b1 = $("#box_32b_code").val();
+
+//box 33 data
+const FL33_a = $("#box_33_code").val();
+const FL33_b = $("#box_33b_code").val();
+const FL33_a1 = $("#box_33_date").val();
+const FL33_b1 = $("#box_33b_date").val();
+
+//box 34 data
+const FL34_a = $("#box_34_code").val();
+const FL34_b = $("#box_34b_code").val();
+const FL34_a1 = $("#box_34_date").val();
+const FL34_b1 = $("#box_34b_date").val();
+
+//box 35 data
+const FL35_a = $("#box_35_code").val();
+const FL35_a1 = $("#box_35_from").val();
+const FL35_a2 = $("#box_35_through").val();
+const FL35_b = $("#box_35b_code").val();
+const FL35_b1 = $("#box_35b_from").val();
+const FL35_b2 = $("#box_35b_through").val();
+
+//box 36 data
+const FL36_a = $("#box_36_code").val();
+const FL36_a1 = $("#box_36_from").val();
+const FL36_a2 = $("#box_36_through").val();
+const FL36_b = $("#box_36b_code").val();
+const FL36_b1 = $("#box_36b_from").val();
+const FL36_b2 = $("#box_36b_through").val();
+
+//box 37 data
+const FL37 = $("#box_37").val();
+const FL37_b = $("#box_37b").val();
+
+//box 38 data
+const FL38Name = $("#box_38_hospital").val();
+const FL38Street = $("#box_38_street").val();
+const FL38City = $("#box_38_city").val();
+const FL38State = $("#box_38_state").val();
+const FL38Zip = $("#box_38_zipcode").val();
+
+//box 39 data
+const FL39_a1 = $("#box_39_code").val();
+const FL39_a2 = $("#box_39_amount").val();
+const FL39_b1 = $("#box_39b_code").val();
+const FL39_b2 = $("#box_39b_amount").val();
+const FL39_c1 = $("#box_39c_code").val();
+const FL39_c2 = $("#box_39c_amount").val();
+const FL39_d1 = $("#box_39d_code").val();
+const FL39_d2 = $("#box_39d_amount").val();
+
+//box 40 data
+const FL40_a1 = $("#box_40_code").val();
+const FL40_a2 = $("#box_40_amount").val();
+const FL40_b1 = $("#box_40b_code").val();
+const FL40_b2 = $("#box_40b_amount").val();
+const FL40_c1 = $("#box_40c_code").val();
+const FL40_c2 = $("#box_40c_amount").val();
+const FL40_d1 = $("#box_40d_code").val();
+const FL40_d2 = $("#box_40d_amount").val();
+
+
+//box 41 data
+const FL41_a1 = $("#box_41_code").val();
+const FL41_a2 = $("#box_41_amount").val();
+const FL41_b1 = $("#box_41b_code").val();
+const FL41_b2 = $("#box_41b_amount").val();
+const FL41_c1 = $("#box_41c_code").val();
+const FL41_c2 = $("#box_41c_amount").val();
+const FL41_d1 = $("#box_41d_code").val();
+const FL41_d2 = $("#box_41d_amount").val();
+
+//box 50 data
+const FL50_a = $("#box_50_A").val();
+const FL50_b = $("#box_50_B").val();
+const FL50_c = $("#box_50_C").val();
+
+//box 51 data
+const FL51_a = $("#box_51_A").val();
+const FL51_b = $("#box_51_B").val();
+const FL51_c = $("#box_51_C").val();
+
+//box 52 data
+const FL52_a = $("#box_52_A").val();
+const FL52_b = $("#box_52_B").val();
+const FL52_c = $("#box_52_C").val();
+
+//box 53 data
+const FL53_a = $("#box_53_A").val();
+const FL53_b = $("#box_53_B").val();
+const FL53_c = $("#box_53_C").val();
+
+//box 54 data
+const FL54_a = $("#box_54_A").val();
+const FL54_b = $("#box_54_B").val();
+const FL54_c = $("#box_54_C").val();
+
+//box 55 data
+
+const FL55_a = $("#box_55_A").val();
+const FL55_b = $("#box_55_B").val();
+const FL55_c = $("#box_55_C").val();
+
+//box 56 data
+const FL56 = $("#box_56_data").val();
+
+
+//box 57 data
+const FL57_a = $("#box_57_A").val();
+const FL57_b = $("#box_57_B").val();
+const FL57_c = $("#box_57_C").val();
+
+
+//box 58 data
+const FL58_a = $("#box_58_insuredName").val();
+const FL58_b = $("#box_58_B").val();
+const FL58_c = $("#box_58_C").val();
+
+
+//box 59 data
+const FL59_a = $("#box_59_A").val();
+const FL59_b = $("#box_59_B").val();
+const FL59_c = $("#box_59_C").val();
+
+//box 60 data
+const FL60_a = $("#box_60_A").val();
+const FL60_b = $("#box_60_B").val();
+const FL60_c = $("#box_60_C").val();
+
+//box 61 data
+const FL61_a = $("#box_61_A").val();
+const FL61_b = $("#box_61_B").val();
+const FL61_c = $("#box_61_C").val();
+
+
+//box 62 data
+const FL62_a = $("#box_62_A").val();
+const FL62_b = $("#box_62_B").val();
+const FL62_c = $("#box_62_C").val();
+
+
+//box 63 data
+const FL63_a = $("#box_63_A").val();
+const FL63_b = $("#box_63_B").val();
+const FL63_c = $("#box_63_C").val();
+
+//box 64 data
+const FL64_a = $("#box_64_A").val();
+const FL64_b = $("#box_64_B").val();
+const FL64_c = $("#box_64_C").val();
+
+//box 65 data
+const FL65_a = $("#box_65_A").val();
+const FL65_b = $("#box_65_B").val();
+const FL65_c = $("#box_65_C").val();
+
+//box 66 data
+const FL66 = $("#box_66_under").val();
+
+
+//box 67 data
+const FL67 = $("#box_67").val();
+const FL67_a = $("#box_67_a").val();
+const FL67_b = $("#box_67_b").val();
+const FL67_c = $("#box_67_c").val();
+const FL67_d = $("#box_67_d").val();
+const FL67_e = $("#box_67_e").val();
+const FL67_f = $("#box_67_f").val();
+const FL67_g = $("#box_67_g").val();
+const FL67_h = $("#box_67_h").val();
+const FL67_i = $("#box_67_i").val();
+const FL67_j = $("#box_67_j").val();
+const FL67_k = $("#box_67_k").val();
+const FL67_l = $("#box_67_l").val();
+const FL67_m = $("#box_67_m").val();
+const FL67_n = $("#box_67_n").val();
+const FL67_o = $("#box_67_o").val();
+const FL67_p = $("#box_67_p").val();
+const FL67_q = $("#box_67_q").val();
+
+//box 68 data
+const FL68 = $("#box_68_up").val();
+const FL68_a = $("#box_68_under").val();
+
+//box 69 data
+const FL69 = $("#box_69_data").val();
+
+//box 70 data
+const FL70_a = $("#box_70_a").val();
+const FL70_b = $("#box_70_b").val();
+const FL70_c = $("#box_70_c").val();
+
+
+//box 71 data
+const FL71 = $("#box_71_data").val();
+
+
+//box 72 data
+const FL72_a = $("#box_72_a").val();
+const FL72_b = $("#box_72_b").val();
+const FL72_c = $("#box_72_c").val();
+
+//box 73 data
+const FL73 = $("#box_73").val();
+
+//box 74 data
+const FL74_1 = $("#box_74_code").val();
+const FL74_2 = $("#box_74_date").val();
+const FL74_a1 = $("#box_74a_code").val();
+const FL74_a2 = $("#box_74a_date").val();
+const FL74_b1 = $("#box_74b_code").val();
+const FL74_b2 = $("#box_74b_date").val();
+const FL74_c1 = $("#box_74c_code").val();
+const FL74_c2 = $("#box_74c_date").val();
+const FL74_d1 = $("#box_74d_code").val();
+const FL74_d2 = $("#box_74d_date").val();
+const FL74_e1 = $("#box_74e_code").val();
+const FL74_e2 = $("#box_74e_date").val();
+
+//box 75 data
+const FL75 = $("#box_75").val();
+
+//box 76 data
+const FL76_a = $("#box_76_npi").val();
+const FL76_b = $("#box_76_qual").val();
+const FL76_c = $("#box_76_num").val();
+const FL76_d = $("#box_76_last").val();
+const FL76_e = $("#box_76_first").val();
+
+//box 77 data
+const FL77_a = $("#box_77_npi").val();
+const FL77_b = $("#box_77_qual").val();
+const FL77_c = $("#box_77_num").val();
+const FL77_d = $("#box_77_last").val();
+const FL77_e = $("#box_77_first").val();
+
+
+//box 78 data
+const FL78 = $("#box_78_other").val();
+const FL78_a = $("#box_78_npi").val();
+const FL78_b = $("#box_78_qual").val();
+const FL78_c = $("#box_78_num").val();
+const FL78_d = $("#box_78_last").val();
+const FL78_e = $("#box_78_first").val();
+
+//box 79 data
+const FL79 = $("#box_79_other").val();
+const FL79_a = $("#box_79_npi").val();
+const FL79_b = $("#box_79_qual").val();
+const FL79_c = $("#box_79_num").val();
+const FL79_d = $("#box_79_last").val();
+const FL79_e = $("#box_79_first").val();
+
+//box 80 data
+const FL80b = $("#box_80b").val();
+const FL80c = $("#box_80c").val();
+const FL80d = $("#box_80d").val();
+
+//box 81 data
+const FL81_a1 = $("#box_81aOne").val();
+const FL81_a2 = $("#box_81aTwo").val();
+const FL81_a3 = $("#box_81aThree").val();
+const FL81_b1 = $("#box_81bOne").val();
+const FL81_b2 = $("#box_81bTwo").val();
+const FL81_b3 = $("#box_81bThree").val();
+const FL81_c1 = $("#box_81cOne").val();
+const FL81_c2 = $("#box_81cTwo").val();
+const FL81_c3 = $("#box_81cThree").val();
+const FL81_d1 = $("#box_81dOne").val();
+const FL81_d2 = $("#box_81dTwo").val();
+const FL81_d3 = $("#box_81dThree").val();
+
